7e1bde3835d512f4bb71383d517a791fedf5ca5b
bappedaabid-hue <bappeda.abid@gmail.com>
2025-08-09 16:43:01 +0700

    Add frontend/pages/index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [datasets, setDatasets] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/datasets")
+      .then((res) => res.json())
+      .then((data) => setDatasets(data));
+  }, []);
   return (
-    <main>
-      <h1>Satu Data Bappeda Kota Prabumulih</h1>
-      <p>Portal integrasi data sektoral Kota Prabumulih.</p>
-    </main>
+    <div>
+      <h1>Daftar Dataset Satu Data Prabumulih</h1>
+      <ul>
+        {datasets.map((ds) => (
+          <li key={ds.id}>{ds.name} ({ds.year})</li>
+        ))}
+      </ul>
+    </div>
   );
 }
