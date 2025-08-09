7fd200aab1dcda1b6cd158599f94f1a6c375a0fa
bappedaabid-hue <bappeda.abid@gmail.com>
2025-08-09 16:39:47 +0700

    Update index.js for frontend page

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Satu Data Bappeda Kota Prabumulih</h1>
+      <p>Portal integrasi data sektoral Kota Prabumulih.</p>
+    </main>
+  );
+}
